ee3b34199acc9fd9363a573a81aef185685e13bf
Danny Van Stemp <danwardvs@adsgames.net>
2015-10-31 20:23:16 -0400

    A few more binary combinations... trial and error

--- main.py
+++ main.py
@@ -71,9 +71,15 @@ def drawNumber(digit1,digit2,digit3,digit4):
 
     time.sleep(speed)
 
-drawNumber(0,0,0,0)
-drawNumber(1,0,0,0)
-drawNumber(1,1,0,0)
+drawNumber(0,0,0,0) ## 6
+drawNumber(1,0,0,0) ## nothing
+drawNumber(1,1,0,0) ## 9
+drawNumber(1,1,1,0) ##
+drawNumber(1,1,1,1)
+drawNumber(0,1,0,0)
+drawNumber(0,1,1,1)
+drawNumber(0,0,1,1)
+
 
     
     
